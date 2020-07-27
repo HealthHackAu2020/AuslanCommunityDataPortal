ed8e465a1788e50c7b99730fd3b58187ac707b18
Chris James <chris.james@oovvuu.com>
2020-07-28 00:06:00 +1000

    chore: add gql and swr libraries
    - also remove unused component library

--- app/components/Select.jsx
+++ app/components/Select.jsx
@@ -10,7 +10,7 @@ export const Select = ({ className, options, children, ...props }) => {
       >
         {options ? renderChildOptions(options) : children}
       </select>
-      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-600">
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-600">
         <CaretDown className="h-8 w-8" />
       </div>
     </div>
