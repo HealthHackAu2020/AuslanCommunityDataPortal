6952d688fe9deb8f35d82badd9d3a55a8bc3847c
Michael Swan <sirsquish@hotmail.com>
2020-08-02 11:31:33 +1000

    Added the terms and conditions (#7)
    Co-authored-by: sirsquish <43261902+sirsquish@users.noreply.github.com>

---
+++ app/pages/tandc.jsx
@@ -0,0 +1,194 @@
+import {Link} from "../components/Link";
+
+export default function tAndC() {
+return(
+
+    <>
+        <div>
+            <Link className="justify-center" href="/">Home</Link>
+            <br/>
+            <br/>
+            <br/>
+            <h1><b>Participant Information</b></h1>
+
+            <p><span>By joining the Auslan Learner Portal, you are invited to submit
+your videos for use in machine learning. This data collection is part of the
+Auslan Communication Technologies Pipeline project, (ethics ID number:
+2020001063), run by Dr Jessica Korte.<br/>
+<br/>
+Please read the following information about the project so that you can decide
+whether you would like to take part in this research. If you have questions,
+please get in touch with Dr Jessica Korte.<br/>
+<br/>
+If you decide to participate in this research, please keep in mind that your
+participation is voluntary. If you do not wish to take part, you do not have
+to. If you decide to take part and later change your mind, you are free to stop
+at any time, and you would not need to give any explanation for your decision to
+stop participating. If you choose to stop participating, your data will no
+longer be used in the research. Your decision whether you take part, or not to
+take part, or to take part and then withdraw, will not affect your relationship
+with the Auslan Learner Portal.<br/>
+<br/>
+You may use the “Print this page” button above to keep a copy of this
+information.<br/>
+<br/>
+<b>What is this research about?</b><br/>
+<br/>
+This project seeks to apply machine learning techniques to Auslan, with the
+intention of creating technologies which can recognise, produce and process
+Auslan. The long-term goal of research like this is to create “Siri for
+Auslan”, with computers, robots and smart devices being able to receive and
+understand instructions via Auslan signs and/or simplified gestures.<br/>
+<br/>
+The goal for Phase 1 of the project is the creation of an open-source dataset
+of Auslan signing videos, collected from professional (i.e. interpreters),
+fluent (i.e. Deaf, CODA) and learner signers. These videos will be used to
+train machine learning models to identify and recognise Auslan signs and
+communication.<br/>
+<br/>
+This research is funded by the Trusted Autonomous Systems Defence Cooperative
+Research Centre (TAS DCRC) and Defence Science and Technology Group (DST).
+Individualised gestural human-machine interfaces (e.g. “Siri for Auslan”) has
+potential applications for control of robots in operational settings.<br/>
+<br/>
+<b>What will I need to do?</b><br/>
+<br/>
+In Phase 1, we need video of you signing in Auslan (either alone or with
+communication partner/s), and your assistance in ensuring our coding
+(translation) of signs is correct.<br/>
+<br/>
+You will be able to do this by uploading videos of yourself signing, and
+providing information about your videos, and other peoples’ videos. The Auslan
+Learner Portal will support you to do this through the learning activities it
+provides.<br/>
+<br/>
+Videos can be captured from a number of angles (full-body front or side-on,
+head-and-torso front or side-on, torso-only front or side-on, hands-only,
+first-person perspective, body-cam view) depending on your level of comfort.<br/>
+<br/>
+We then will need you to choose how you want us to use the video:<br/>
+1. Release it as part of the open-source Auslan dataset; or<br/>
+2. Use it only in machine learning training related to this project.<br/>
+You can do this from your profile, and from each video individually.<br/>
+<br/>
+<b>What are the possible benefits of taking part?</b><br/>
+<br/>
+As an Auslan Learner: There are no particular benefits to you from providing
+your data for machine learning. Using the Auslan Learner Portal may help you to
+improve your fluency of Auslan.<br/>
+<br/>
+For the Deaf community: Success in this project and related projects may result
+in technologies which can be accessed and controlled through Auslan signing.
+Possibly creation of automated English-Auslan interpretation technologies.<br/>
+<br/>
+For technology and robotics producers, including TAS DCRC and DST Group:
+Gesture-based human-machine interfaces, informed by expert visual-gestural
+communicators (i.e. fluent and professional Auslan signers).<br/>
+<br/>
+<b>What are the possible risks and disadvantages of taking part?</b><br/>
+<br/>
+If your videos are included in the open-source dataset, images of your face,
+hands and signing will be available online. To mitigate risks associated with
+this:<br/>
+<br/>
+1. You choose whether or not your videos are included in the open-source
+dataset.<br/>
+<br/>
+2. You choose what angle/s your videos will capture, allowing you to exclude
+your face if you prefer.<br/>
+<br/>
+3. The only information about you that will be associated with any video is
+your level of fluency.<br/>
+<br/>
+4. The Auslan signing videos should not include any private or sensitive
+information.<br/>
+<br/>
+5. You retain the on-going right to request that a video which is clearly
+identifiable as you (i.e. includes your face, or has been uploaded from your
+account) be removed from the open-source dataset.<br/>
+<br/>
+6. Experts and students who work on this project and related projects will have
+terms of appropriate use.<br/>
+<br/>
+7. The open-source license under which the dataset is published will set out
+terms of appropriate use, including limiting use of the dataset to sign
+language and gesture related projects.<br/>
+<br/>
+8. If you are under 18 years of age, you will not by default be able to include
+your videos in the open source dataset. If you wish them to be included, please
+ask your parent or guardian to get in touch with Dr Jessica Korte.<br/>
+<br/>
+<b>What will happen to information about me?</b><br/>
+<br/>
+We are not collecting information about you. We are collecting examples of
+Auslan signs and conversations, as they are used in the real world. Anonymity
+may not be possible, depending on whether or not you choose to include your
+face in your videos, but no identifying information will be attached to your
+videos, and videos should not include any sensitive or private information.<br/>
+<br/>
+If you choose for your videos to be available to the Auslan Communication
+Technologies Pipeline project, but not included in the open-source dataset,
+they will remain confidential to the team working on this project and related
+projects.<br/>
+<br/>
+All reasonable precautions will be taken to ensure your videos are  stored
+securely.<br/>
+<br/>
+If you choose for your videos to be available to the Auslan Communication
+Technologies Pipeline project, they may be accessed by people associated with
+this project. This may include:<br/>
+•  Dr Jessica Korte, the project lead;<br/>
+•  Other academics, researchers, data scientists, machine learning experts and
+DST group staff working on this project or related projects;<br/>
+•  Animators creating resources for Auslan production;<br/>
+•  Students working on this project or related projects; and<br/>
+•  Professional signers, fluent signers, and Auslan linguists involved in
+coding/translating videos.<br/>
+<br/>
+Videos published in the open-source dataset will be released under a license
+such as the Open Data Commons Attribution license with conditions that it is
+only to be used for purposes related to sign language and gesture.<br/>
+<br/>
+It is anticipated that the results of this research project will be published
+and/or presented in a variety of forms. In any publication and/or presentation,
+information will be provided in such a way that you cannot be identified,
+except with your expressed permission.<br/>
+<br/>
+Records including recordings will be retained in accordance with the
+obligations under the Public Records Act 2002 and associated State policies,
+retention and disposal schedules and other official advice issued by the
+Queensland State Archives’ State Archivist.<br/>
+<br/>
+<b>What will happen if I decide to withdraw?</b><br/>
+<br/>
+Your participation in this research is voluntary and you are free to withdraw
+from the research anytime without needing to provide any explanation, and you
+will not be disadvantaged in any way if you withdraw. Should you decide to
+withdraw, all identifiable videos will be destroyed and will no longer be used
+in the research.<br/>
+<br/>
+<b>Can I learn about the results of this research?</b><br/>
+<br/>
+To get updates on this research, including announcements of publications and
+new Phases, please provide your contact details on the consent sheet.<br/>
+<br/>
+<b>Who can I contact if I have any concerns about the project?</b><br/>
+<br/>
+This study adheres to the Guidelines of the ethical review process of The
+University of Queensland and the National Statement on Ethical Conduct in Human
+Research. Whilst you are free to discuss your participation in this study with
+the researcher contactable at j.korte@uq.edu.au, if you would like to speak to
+an officer of the University not involved in the study, you may contact the Ethics
+Coordinator on +61 7 3365 3924/ +61 7 3443 1656 or email
+    humanethics@research.uq.edu.au. </span></p>
+
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <Link className="justify-center" href="/">Home</Link>
+    </>
+
+
+)
+}
